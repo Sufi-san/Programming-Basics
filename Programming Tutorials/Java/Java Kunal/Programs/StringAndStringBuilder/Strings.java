f5cd551a7d156dbe8e514c9e0243da847a08ae93
Sufiyan <sufy_456@hotmail.com>
2023-02-02 21:05:08 +0530

    Started Strings: Intro to String Immutability

---
+++ Programming Tutorials/Java/Java Kunal/Programs/StringAndStringBuilder/Strings.java
@@ -0,0 +1,36 @@
+package StringAndStringBuilder;
+import java.util.Arrays;
+public class Strings {
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5};  // can be changed
+        arr[2] = 8;
+        System.out.println(Arrays.toString(arr));
+        String name = "Sufiyan Chougule";  // Immutable
+        System.out.println(name);  // Simple Printing
+
+        // String is the most commonly used class in java class library
+        // Everything that starts with a capital letter at the beginning is a class.
+        // Every String we create is an object of type String.
+
+        // About String Immutability:
+        /* Concepts:
+            1) String Pool: It is a separate memory structure inside the heap containing the objects
+                            at which the reference variable points.
+                            All Similar values of String are not recreated in the pool, Saving memory.
+                            This makes program more optimised.
+            2) String Immutability:  Even though reference variables point to the same object in String pool,
+                                     unlike arrays, the object cannot be modified and hence there can be no
+                                     change made via any reference variable.
+                                     In case we change an old reference variable, a new object will be created
+                                     in the pool, and it will point to that new object instead of changing the
+                                     old one. The old object will then be erased by the garbage collector.
+         */
+        String name1 = "Shubham";
+        String name2 = "Shubham";  // Pointing to same 'Shubham' in String Pool
+        System.out.println("name1: "+name1+" & name2: "+name2+" are the same object in String Pool.");
+        // Why are Strings Immutable?
+        // Short Answer: for security
+        /* If strings were immutable, then if one string object to which different objects were pointing
+           was changed, then it would have been changed for all the reference variables. */
+    }
+}
